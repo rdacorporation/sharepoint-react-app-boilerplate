660ebba02611d95cbae335576110c56d9827fb07
Sean McLellan <sean.mclellan@sixconcepts.com>
2018-11-28 15:40:18 -0500

    Some changes

--- bin/sp-rest-proxy.js
+++ bin/sp-rest-proxy.js
@@ -1,27 +1,24 @@
-"use strict";
+'use strict';
 
-const runAll = require("npm-run-all");
+const runAll = require('npm-run-all');
 
 const args = process.argv.slice(2);
-const scriptIndex = args.findIndex(
-  x => x === "build" || x === "start" || x === "test"
-);
+const scriptIndex = args.findIndex(x => x === 'build' || x === 'start' || x === 'test');
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 
-require("dotenv").config();
-var RestProxy = require("sp-rest-proxy/dist/RestProxy");
+require('dotenv').config();
+var RestProxy = require('sp-rest-proxy/dist/RestProxy');
 var settings = {
-  configPath:
-    process.env.SP_Rest_Proxy_ConfigPath || "./sp-rest-proxy/private.json",
+  configPath: process.env.SP_Rest_Proxy_ConfigPath || './sp-rest-proxy/private.json',
   port: process.env.SP_Rest_Proxy_Port || 3001,
-  staticRoot: process.env.SP_Rest_Proxy_StaticRoot || "./static"
+  staticRoot: process.env.SP_Rest_Proxy_StaticRoot || './static'
 };
 var proxy = new RestProxy.default(settings);
 proxy.serve((server, context, settings) => {
   switch (script) {
-    case "build":
-    case "start":
-    case "test":
+    case 'build':
+    case 'start':
+    case 'test':
       runAll([`${script}-*`], {
         parallel: true,
         stdout: process.stdout,
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   async componentDidMount() {
-    console.dir("hi");
+    console.dir('hi');
   }
 
   render() {
@@ -15,12 +15,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
