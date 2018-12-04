cb5b84ab58f3d82a2164853b5cde5ec61ffc3665
Sean McLellan <sean@baristalabs.io>
2018-12-03 23:32:08 -0500

    Add craco-raw-loader

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,15 @@
 const { whenDev } = require('@craco/craco');
+const CracoRawLoaderPlugin = require('@baristalabs/craco-raw-loader');
 
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoRawLoaderPlugin,
+      options: {
+        test: /\.foo$/
+      }
+    }
+  ],
   devServer: whenDev(() => {
     return {
       proxy: [
