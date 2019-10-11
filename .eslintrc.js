453f5b7759f92e6c283b0406220ee1e9e6178e59
Sean McLellan <sean@baristalabs.io>
2019-10-10 21:59:33 -0400

    Disable typescript-eslint rules for .js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,8 @@ module.exports = {
     react: {
       version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     }
+  },
+  overrides: {
+    files: ['*.js']
   }
 };
