7426c09f78932238617e8e10b13bd362d0a9c299
Sean McLellan <sean@baristalabs.io>
2019-10-29 18:48:42 -0400

    Bump dependencies and update polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import 'babel-polyfill';
-import 'whatwg-fetch';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
